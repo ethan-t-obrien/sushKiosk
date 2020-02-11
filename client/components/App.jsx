c752ecb7f1101e8f6f1dd00c31b9dacfcfc229f8
ethan-t-obrien <ethanobrien1009@gmail.com>
2020-02-11 19:26:18 +1300

    inital setup and home page start

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+    <h1 className="title">Sugoi Sushi</h1>
+    <div className="home">
+      <div className="dine">
+        <img src="./imgs/chicken.jpg" alt="chicken sushi"/>
+        <h2 className="dine__head">Dine In</h2>
+      </div>
+      <div className="take">
+        <img src="./imgs/sashimi.jpg" alt="fish sushi"/>
+        <h2 className="take__head">Take Away</h2>
+      </div>
+    </div>
+    </>
   )
 }
 
